db1e9d63c46dd96046e476f109fd1285ff69a286
Claire Ellul <ucfscde@ucl.ac.uk>
2019-03-11 12:43:12 -0400

    full update for DIVchange

--- ucfs114/www/js/rhMenu.js
+++ ucfs114/www/js/rhMenu.js
@@ -1,6 +1,10 @@
-﻿
-	function menuClicked() 
-	{
-			alert("You clicked the menu");
-	}
-	
+//show an alert where click on menu div
+function menuClicked() 
+{
+	alert("You clicked the menu");
+}
+//div change to ucl logo where click on the menu
+function addUCLLogo()
+{
+	document.getElementById('ucllogo').innerHTML="<img src='images/ucl.png'>"
+}
